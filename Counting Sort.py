85775310e5201cd131d168b6ded320ce142c5575
BlessedGT <133872895+BlessedGT@users.noreply.github.com>
2024-04-01 16:04:43 +0300

    Create Counting Sort.py

---
+++ Counting Sort.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    count_arr = [0]*100
+    for item in arr:
+        count_arr[item] += 1
+    return count_arr
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
